592f7f2884928ce59fba89d726441fecf4ebf473
Ramcharxn <ramcharxn@gmail.com>
2021-01-06 15:36:05 +0530

    adding files

---
+++ public/js/main.js
@@ -0,0 +1,70 @@
+const chatForm = document.getElementById('chat-form')
+const chatMessage = document.querySelector('.chat-messages')
+const roomName = document.getElementById('room-name')
+const userList = document.getElementById('users')
+
+// Get username and room from url
+const { username,room } = Qs.parse(location.search,{
+    ignoreQueryPrefix:true
+})
+
+const socket = io();
+
+// Join chat
+socket.emit('joinRoom',{ username,room })
+
+// Get room and users
+socket.on('roomUsers',({
+    room,
+    users
+})=>{
+    outputRoomName(room),
+    outputUsers(users)
+})
+
+// Message from server
+socket.on('message', message =>{
+    console.log(message)
+    outputMessage(message)
+
+    // Scroll down
+    chatMessage.scrollTop = chatMessage.scrollHeight
+})
+
+// message submit
+chatForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    // get message from form
+    const msg= e.target.elements.msg.value;
+
+    // Emit a message to server
+    socket.emit('chatMessage',msg)
+
+    // Clear msg
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus()
+})
+
+// Output messae from DOM
+function outputMessage(message){
+    const div = document.createElement('div')
+    div.classList.add('message')
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`
+    document.querySelector('.chat-messages').appendChild(div)
+}
+
+// Add room name to DOM
+function outputRoomName(room){
+    roomName.innerText = room
+}
+
+// Add user name
+function outputUsers(users){
+    userList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join('')}
+    `
+}
